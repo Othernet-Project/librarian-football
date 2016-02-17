9d45eb1db0b652486d02ea234518601b409ec996
George Deamont <deamontg@gmail.com>
2016-02-16 20:04:50 -0800

    No longer using xmlsoccer

--- librarian_football/routes.py
+++ librarian_football/routes.py
@@ -3,7 +3,7 @@ import os
 from bottle import request
 from bottle_utils.i18n import i18n_url
 from librarian_core.contrib.templates.renderer import view
-from adapters import footballdata as adapter
+from adapters import football_data as adapter
 
 
 EXPORTS = {
@@ -13,7 +13,7 @@ EXPORTS = {
 
 @view('football/football')
 def football_scores():
-    key = request.app.config['football.footballdata_key']
+    key = request.app.config['app.football_data_key']
     leagues = adapter.get_all_leagues(key)
     leagues.sort(key=lambda x: x.name)
 
