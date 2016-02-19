3653b148fa7f5c87bb2c3f326c4c5e1dc2817124
George Deamont <deamontg@gmail.com>
2016-02-18 19:06:55 -0800

    Starting from fresh w/ FSAL stuff

--- librarian_football/menuitems.py
+++ librarian_football/menuitems.py
@@ -1,5 +1,4 @@
 from bottle_utils.i18n import lazy_gettext as _
-
 from librarian_menu.menu import MenuItem
 
 
--- librarian_football/routes.py
+++ librarian_football/routes.py
@@ -1,9 +1,6 @@
-import os
-
 from bottle import request
 from bottle_utils.i18n import i18n_url
 from librarian_core.contrib.templates.renderer import view
-from adapters.football_data import League
 
 
 EXPORTS = {
@@ -13,18 +10,7 @@ EXPORTS = {
 
 @view('football/football')
 def football_scores():
-    db = request.db['football']
-    q = db.Select(sets='leagues')
-    leagues_db = db.fetchall(q)
-
     leagues = []
-    for l in leagues_db:
-        name = l[1]
-        league = League(name, None)
-        leagues.append(league)
-
-    leagues.sort(key=lambda x: x.name)
-
     return dict(leagues=leagues,
     	        base_path=i18n_url('football'),
                 view=request.params.get('view'))
