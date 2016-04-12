b1c89c2024b259e2a461e6b5984bfe549dbc507d
George Deamont <deamontg@gmail.com>
2016-04-12 08:47:54 -0700

    Add test definitions

---
+++ tests/test_football.py
@@ -0,0 +1,15 @@
+import mock
+
+from librarian_football import football as mod
+
+
+def test_get_leagues():
+    pass
+
+
+def test_get_league():
+    pass
+
+
+def test_get_leagues_count():
+    pass
